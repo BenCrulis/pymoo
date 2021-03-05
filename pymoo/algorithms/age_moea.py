41851cf98c5e64c5ec44a8f09bbd36ebf2e00d26
bencrulis@gmail.com <afwpzx18725vmh>
2021-03-05 10:30:50 +0100

    Added exception handling when matrix is singular in hyperplane calculation. Revert to secondary method of normalization.

--- pymoo/algorithms/age_moea.py
+++ pymoo/algorithms/age_moea.py
@@ -180,13 +180,18 @@ def normalize(front, extreme):
     # Calculate the intercepts of the hyperplane constructed by the extreme
     # points and the axes
 
-    hyperplane = np.linalg.solve(front[extreme], np.ones(n))
-    if any(np.isnan(hyperplane)) or any(np.isinf(hyperplane)) or any(hyperplane < 0):
-        normalization = np.max(front, axis=0)
-    else:
-        normalization = 1. / hyperplane
-        if any(np.isnan(normalization)) or any(np.isinf(normalization)):
+    try:
+        hyperplane = np.linalg.solve(front[extreme], np.ones(n))
+        if any(np.isnan(hyperplane)) or any(np.isinf(hyperplane)) or any(hyperplane < 0):
             normalization = np.max(front, axis=0)
+        else:
+            normalization = 1. / hyperplane
+            if any(np.isnan(normalization)) or any(np.isinf(normalization)):
+                normalization = np.max(front, axis=0)
+    except np.linalg.LinAlgError:
+        normalization = np.max(front, axis=0)
+
+    normalization[normalization == 0.0] = 1.0
 
     # Normalization
     front = front / normalization
